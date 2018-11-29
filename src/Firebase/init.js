523e0a78fc288739af4322de17174abe3c2c47fc
irakli-dz <i.dzigrashvili@gmail.com>
2018-11-29 14:20:46 +0400

    First commit

---
+++ src/Firebase/init.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCxaZaHS9HfRKub1E5RmxT12jCiCGpi9mM",
+    authDomain: "smoothies-e64fb.firebaseapp.com",
+    databaseURL: "https://smoothies-e64fb.firebaseio.com",
+    projectId: "smoothies-e64fb",
+    storageBucket: "smoothies-e64fb.appspot.com",
+    messagingSenderId: "6797556539"
+  };
+  firebase.initializeApp(config);
